ca6e5c018eb65d3723f91fc74b1a10ee55de862f
BLACKxOw1 <nuriddintuychiyev5@gmail.com>
2025-10-06 20:51:10 +0500

    hello

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,15 @@
+import React from "react"
+import Header from "./components/header"
+import Footer from "./components/footer"
 
-function App() {
-  <h1>hello</h1>
+function App(){
+  return (
+    <React.Fragment>
+
+    <Header />
+
+    </React.Fragment>
+  )
 }
 
 export default App
---
+++ src/components/header.jsx
@@ -0,0 +1,14 @@
+
+function Header() {
+  return <div className="container bg-zinc-800 w-[100px] h-[100px]">
+    <img src="../images/Logo.svg" alt="" />
+    <div>salom</div>
+    <div className="flex">
+      <img src="" alt="" />
+      <img src="" alt="" />
+      <img src="" alt="" />
+    </div>
+  </div>
+}
+
+export default Header
